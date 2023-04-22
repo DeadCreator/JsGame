294b2d675a7329341a3caad419a58a168561a58d
TheGeniusOfTheGame <maxsafin14@gmail.com>
2023-04-22 08:34:55 +0300

    version 1.7 - Better Gravity

--- script.js
+++ script.js
@@ -7,13 +7,49 @@ const gameWindow = document.querySelector(".game-window")
 const menuWindow = document.querySelector(".game-menu")
 const info = document.querySelector(".info")
 const enemyBottom = 20
-let enemyX = 300
+let entityX = 300
 let playerX = 30
 const playerWidth = 8
 const enemyWidth = 8
 let gameStarted = false
 let playerBottom = 20
 let isJumping = false
+let speed = 100
+let t = Date.now()
+let timePassed
+let movement = 0
+
+const Move = () => {
+
+    timePassed = (Date.now() - t) / 1000
+    t = Date.now()
+    let fps = Math.round(1 / timePassed)
+    info.innerHTML = `FPS: ${fps}`
+
+    entityX -= (speed * timePassed)
+    background.style.backgroundPositionX = entityX + "vh"
+    ground.style.backgroundPositionX = entityX + "vh"
+    enemy.style.left = entityX + "vh"
+
+
+    if ((entityX <= playerX + playerWidth) &&
+        (playerBottom <= enemyBottom + 9) &&
+        (entityX + enemyWidth >= playerX)) {
+        console.log('stop', playerBottom)
+        gameStarted = false
+        gameOver()
+    }
+
+    if (entityX <= -20) {
+        entityX = 300
+    }
+
+    if (gameStarted) {
+        window.requestAnimationFrame(Move)
+        window.requestAnimationFrame(gravity)
+    }
+};
+
 
 startGame.addEventListener("click", () => {
     menuWindow.style.display = "none"
@@ -21,70 +57,44 @@ startGame.addEventListener("click", () => {
     player.style.bottom = 20 + "vh"
     gameStarted = true
     Move()
+    gravity()
 })
 
 const jump = async () => {
-    if (!isJumping && gameStarted) {
+    if (!isJumping) {
         isJumping = true
         let timerUpId = setInterval(() => {
+            if (playerBottom >= 44 || !gameStarted) {
+                clearInterval(timerUpId)
+                isJumping = false
+            }
+
             playerBottom += 6
             playerBottom *= 0.90
             player.style.bottom = playerBottom + 'vh'
 
-            if (playerBottom >= 44) {
-                clearInterval(timerUpId)
-                playerBottom = Math.floor(playerBottom)
-                let timerDownId = setInterval(() => {
-                    playerBottom -= 1.5
-                    player.style.bottom = playerBottom + 'vh'
-                    if (playerBottom <= 20 || !gameStarted) {
-                        clearInterval(timerDownId)
-                        isJumping = false
-                    }
-                }, 20)
-            }
         }, 20)
     }
 }
 
-let backgroundX = 0
-let speed = 100
-let t = Date.now()
-
-const Move = () => {
-
-    let timePassed = (Date.now() - t) / 1000
-    t = Date.now()
-    let fps = Math.round(1 / timePassed)
-    info.innerHTML = `FPS: ${fps}`
-
-    backgroundX -= (speed * timePassed)
-    background.style.backgroundPositionX = backgroundX + "vh"
-    ground.style.backgroundPositionX = backgroundX + "vh"
-    enemy.style.left = backgroundX + "vh"
-
-    if ((enemyX <= playerX + playerWidth) &&
-        (playerBottom <= enemyBottom + 9) &&
-        (enemyX + enemyWidth >= playerX)) {
-
-        gameStarted = false
-        player.style.bottom = 20 + "vh"
-        return
+const gravity = () => {
+    if (playerBottom > 20 && !isJumping) {
+        playerBottom -= 1.3
+        player.style.bottom = Math.floor(playerBottom) + 1 + "vh"
     }
+}
 
 
 
-    if (backgroundX <= -10) {
-        backgroundX = 315
-        enemy.style.left = Math.random()
-    }
 
-
-    window.requestAnimationFrame(Move)
-};
+const gameOver = () => {
+    console.log('shirt')
+    menuWindow.style.display = "flex"
+    startGame.innerHTML = "Restart Game"
+}
 
 gameWindow.addEventListener("click", () => {
-    if (!isJumping) {
+    if (!isJumping && gameStarted && playerBottom <= 20) {
         jump()
     }
 })
