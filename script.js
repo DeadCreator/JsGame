89c64fd3914bb882c1371795e5fc259f49b66243
Web-разработчик <web2021-05@int.adtspb.ru>
2023-04-22 19:40:30 +0300

    version 1.8 UI Improvements

--- script.js
+++ script.js
@@ -5,66 +5,112 @@ const ground = document.querySelector(".ground")
 const background = document.querySelector(".background")
 const gameWindow = document.querySelector(".game-window")
 const menuWindow = document.querySelector(".game-menu")
-const info = document.querySelector(".info")
+//const info = document.querySelector(".info")
+const scoreInfo = document.querySelector(".score")
+const scoreKeyFrames = new KeyframeEffect(
+    scoreInfo,
+    [
+        { color: "white" },
+        { color: "lime" },
+        { color: "white" },
+        { color: "lime" },
+        { color: "white" }
+    ],
+    { duration: 2000, fill: "forwards"}
+)
+const scoreAnim = new Animation(
+    scoreKeyFrames,
+    document.timeline
+)
+
+const playerKeyFrames = new KeyframeEffect(
+    player,
+    [
+        { height: "6.5vh" },
+        { height: "8vh" },
+        { height: "6.5vh" },
+        { height: "8vh" }
+    ],
+    { duration: 1000, fill: "forwards"}
+)
+const playerAnim = new Animation(
+    playerKeyFrames,
+    document.timeline
+)
+
 const enemyBottom = 20
+const playerWidth = 8
+let playerHeight = 8
+const enemyWidth = 8
+
 let entityX = 300
 let playerX = 30
-const playerWidth = 8
-const enemyWidth = 8
 let gameStarted = false
 let playerBottom = 20
 let isJumping = false
 let speed = 100
 let t = Date.now()
 let timePassed
+let backgroundX = 300
 let movement = 0
-
-const Move = () => {
-
-    timePassed = (Date.now() - t) / 1000
-    t = Date.now()
-    let fps = Math.round(1 / timePassed)
-    info.innerHTML = `FPS: ${fps}`
-
-    entityX -= (speed * timePassed)
-    background.style.backgroundPositionX = entityX + "vh"
-    ground.style.backgroundPositionX = entityX + "vh"
-    enemy.style.left = entityX + "vh"
-
-
-    if ((entityX <= playerX + playerWidth) &&
-        (playerBottom <= enemyBottom + 9) &&
-        (entityX + enemyWidth >= playerX)) {
-        console.log('stop', playerBottom)
-        gameStarted = false
-        gameOver()
-    }
-
-    if (entityX <= -20) {
-        entityX = 300
-    }
-
-    if (gameStarted) {
-        window.requestAnimationFrame(Move)
-        window.requestAnimationFrame(gravity)
-    }
-};
-
+let score = 0
 
 startGame.addEventListener("click", () => {
-    menuWindow.style.display = "none"
     enemy.style.left = 300 + "vh"
     player.style.bottom = 20 + "vh"
+    menuWindow.classList.add("no-blur")
+    score = 0
     gameStarted = true
     Move()
     gravity()
 })
 
+const Move = () => {
+    score += 1
+    scoreInfo.innerHTML = `Score: ${Math.floor(score / 10)}`
+
+
+    if ((score / 10 ) % 100 == 0) {
+        scoreAnim.play()
+    }
+
+    timePassed = (Date.now() - t) / 1000
+    t = Date.now()
+    let fps = Math.round(1 / timePassed)
+    //info.innerHTML = `FPS: ${fps}`
+
+    entityX -= (speed * timePassed)
+    ground.style.backgroundPositionX = entityX + "vh"
+    enemy.style.left = entityX + "vh"
+    playerHeight = player.style.height
+    console.log(playerHeight)
+
+
+    if ((entityX <= playerX + playerWidth) &&
+        (playerBottom <= enemyBottom + 9) &&
+        (entityX + enemyWidth >= playerX)) {
+        gameStarted = false
+        gameOver()
+    }
+
+    if (entityX <= -20) {
+        entityX = 300 + Math.random() * 50
+    }
+
+    if (gameStarted) {
+        window.requestAnimationFrame(Move)
+        window.requestAnimationFrame(gravity)
+        background.style.backgroundPositionX = backgroundX + "vh"
+        backgroundX -= 0.1
+    }
+};
+
 const jump = async () => {
     if (!isJumping) {
         isJumping = true
+        playerAnim.play()
         let timerUpId = setInterval(() => {
-            if (playerBottom >= 44 || !gameStarted) {
+            if (playerBottom >= 40 || !gameStarted) {
                 clearInterval(timerUpId)
                 isJumping = false
             }
@@ -79,8 +125,16 @@ const jump = async () => {
 
 const gravity = () => {
     if (playerBottom > 20 && !isJumping) {
-        playerBottom -= 1.3
+        playerBottom -= 0.2
+        playerBottom /= 1.03
         player.style.bottom = Math.floor(playerBottom) + 1 + "vh"
+    }
+    if (isJumping) {
+        player.classList.add("squash")
+    }
+
+    if (playerBottom <= 20) {
+        player.classList.remove("squash")
     }
 }
 
@@ -90,6 +144,7 @@ const gravity = () => {
 const gameOver = () => {
     console.log('shirt')
     menuWindow.style.display = "flex"
+    menuWindow.classList.remove("no-blur")
     startGame.innerHTML = "Restart Game"
 }
 
